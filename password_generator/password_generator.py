b014d0bc3a3082627f3f6ca66adbbd937062d0ff
Rohan-R07 <rveda013@gmail.com>
2026-03-12 07:37:07 +0530

    pswd generator

---
+++ password_generator/password_generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+length = int(input("Enter Length: "))
+
+characters = string.ascii_letters+string.digits+string.punctuation
+password=""
+for i in range(length):
+    password+=random.choice(characters)
+print("Generated password: ",password)
